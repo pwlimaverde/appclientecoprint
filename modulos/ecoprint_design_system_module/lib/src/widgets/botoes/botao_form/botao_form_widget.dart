bde350bcc81fcb423d160cd92378dd59a47bc6f1
Paulo Weslley <vorfast@gmail.com>
2021-12-21 19:53:44 -0300

    ajuste wigets form

--- modulos/ecoprint_design_system_module/lib/src/widgets/botoes/botao_form/botao_form_widget.dart
+++ modulos/ecoprint_design_system_module/lib/src/widgets/botoes/botao_form/botao_form_widget.dart
@@ -1,10 +1,14 @@
 import 'package:dependency_module/dependency_module.dart';
-import 'package:ecoprint_design_system_module/src/widgets/forms/form_geral/form_geral_widget.dart';
 import 'package:flutter/material.dart';
 
 class BotaoForm extends StatelessWidget {
   final Widget form;
-  const BotaoForm({Key? key}) : super(key: key);
+  final Widget button;
+  const BotaoForm({
+    Key? key,
+    required this.form,
+    required this.button,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -22,18 +26,7 @@ class BotaoForm extends StatelessWidget {
             padding: const EdgeInsets.all(0),
             width: 20,
             height: 30,
-            child: IconButton(
-              padding: const EdgeInsets.all(0),
-              alignment: Alignment.centerLeft,
-              icon: const Icon(
-                Icons.clear,
-                color: Colors.red,
-                size: 20,
-              ),
-              onPressed: () {
-                opsController.limparBusca();
-              },
-            ),
+            child: button,
           ),
         ],
       ),
